8ced932bd2d1200d785b46176afce79a54a61a31
Andy Morris <andrew@missionlabs.co.uk>
2020-10-16 11:42:55 +0100

    Add gatsby-config and basic components

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: `Zeta Electrical`,
+    description: `Zeta Electrical. For all your electrical needs.`,
+    author: `Mogg`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+  ],
+}
